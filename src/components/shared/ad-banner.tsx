53d02c5d1e7aad5759a35fd3062dbc84251773e1
rajeev singh <singhrajeev1470@gmail.com>
2025-11-17 15:28:38 +0000

    remove this <ins class="adsbygoogle" style="display:block" data-ad-clien

--- src/components/shared/ad-banner.tsx
+++ src/components/shared/ad-banner.tsx
@@ -29,15 +29,5 @@ export function AdBanner({ adSlot, adFormat = "auto", dataFullWidthResponsive = 
     }
   }, [pathname, adSlot]);
 
-  return (
-      <ins
-        className={cn('adsbygoogle', className)}
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-3080938150148610"
-        data-ad-slot={adSlot}
-        data-ad-format={adFormat}
-        data-full-width-responsive={dataFullWidthResponsive.toString()}
-        {...props}
-      ></ins>
-  );
+  return null;
 }
