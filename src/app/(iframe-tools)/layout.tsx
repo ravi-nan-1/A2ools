4d78f4501bfd250bab828f614041cccd679aec8c
rajeev singh <singhrajeev1470@gmail.com>
2025-12-02 20:08:44 +0000

    Try fixing this error: `Console Error: In HTML, <html> cannot be a child

--- src/app/(iframe-tools)/layout.tsx
+++ src/app/(iframe-tools)/layout.tsx
@@ -1,15 +1,7 @@
-import '../globals.css';
-
 export default function IframeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <div className="w-full h-screen">{children}</div>
-      </body>
-    </html>
-  );
+  return <div className="w-full h-screen">{children}</div>;
 }
